52d1d55912ff05a9d0bfd4cbfdd30c9e6b8aaff7
WhiteW1ine <foxratmir2000@gmail.com>
2020-06-07 03:00:16 +0200

    Update

--- 5SD803-Assignment1-DataStructures-2019/source/linked_list.cpp
+++ 5SD803-Assignment1-DataStructures-2019/source/linked_list.cpp
@@ -25,11 +25,34 @@ linked_list::~linked_list()
 {
 }
 
-void linked_list::push_front(int value) {
-
+void linked_list::push_front(int value) 
+{
+    node* new_node = new node(value, nullptr);
+    if (head_ == nullptr)
+    {
+        head_ = new_node;
+        count_++;
+    }
+    else if (head_ != nullptr)
+    {
+        new_node->next_ = head_;
+        head_ = new_node;
+        count_++;
+    }
 }
 
-void linked_list::push_back(int value) {
+void linked_list::push_back(int value) 
+{
+    node* new_node = new node(value, nullptr);
+    if (head_ == nullptr)
+    {
+        head_ = new_node;
+        count_++;
+    }
+    else
+    {
+
+    }
 
 }
 
@@ -58,13 +81,27 @@ int linked_list::size() const {
 }
 
 int linked_list::front() const {
-   return -1;
+   return head_->value_;
 }
 
-int linked_list::back() const {
-   return -1;
+int linked_list::back() const 
+{
+    node* current = new node;
+    current = head_;
+    while (current->next_ != nullptr)
+    {
+        current = current->next_;
+    }
+   return current->value_;
 }
 
-int linked_list::at(int index) const {
-   return -1;
+int linked_list::at(int index) const 
+{
+    node* current = new node;
+    current = head_;
+    for (int i = 0; i < index; i++)
+    {
+        current = current->next_;
+    }
+    return current->value_;
 }
